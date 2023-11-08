0c8656ad5b217dfbb80fe4b0bcd335f33bab818d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-11-08 13:57:33 +0200

    Update solution_test.rb

--- src/test/ruby/g0001_0100/s0001_two_sum/solution_test.rb
+++ src/test/ruby/g0001_0100/s0001_two_sum/solution_test.rb
@@ -3,18 +3,18 @@ require 'minitest/autorun'
 
 class TestTwoSum < Minitest::Test
   def test_two_sum
-    assert_equal [0, 1], twoSum([2, 7, 11, 15], 9)
+    assert_equal [0, 1], two_sum([2, 7, 11, 15], 9)
   end
 
   def test_two_sum2
-    assert_equal [1, 2], twoSum([3, 2, 4], 6)
+    assert_equal [1, 2], two_sum([3, 2, 4], 6)
   end
 
   def test_two_sum3
-    assert_equal [0, 1], twoSum([3, 3], 6)
+    assert_equal [0, 1], two_sum([3, 3], 6)
   end
 
   def test_two_sum4
-    assert_equal [-1, -1], twoSum([3, 3], 7)
+    assert_equal [-1, -1], two_sum([3, 3], 7)
   end
 end
