987112a0a8ac2276eea6e34a3aabb87a64c379c1
Valentyn Kolesnikov <javadev75@gmail.com>
2023-11-27 05:26:38 +0200

    Improved task 138

--- src/test/ruby/g0101_0200/s0138_copy_list_with_random_pointer/solution_test.rb
+++ src/test/ruby/g0101_0200/s0138_copy_list_with_random_pointer/solution_test.rb
@@ -3,7 +3,7 @@ require 'minitest/autorun'
 require_relative '../../../../main/ruby/g0101_0200/s0138_copy_list_with_random_pointer/solution'
 
 class SolutionTest < Minitest::Test
-  def test_copy_random_list
+  def test_copyRandomList
     node7 = Node.new(7)
     node13 = Node.new(13)
     node11 = Node.new(11)
@@ -20,10 +20,10 @@ class SolutionTest < Minitest::Test
     node10.random = node11
     node1.random = node7
     expected_result = '[[7, nil],[13, 0],[11, 4],[10, 2],[1, 0]]'
-    assert_equal expected_result, copy_random_list(node7).to_s
+    assert_equal expected_result, copyRandomList(node7).to_s
   end
 
-  def test_copy_random_list_2
+  def test_copyRandomList_2
     node1 = Node.new(1)
     node2 = Node.new(2)
     node1.next = node2
@@ -31,10 +31,10 @@ class SolutionTest < Minitest::Test
     node2.next = nil
     node2.random = node2
     expected_result = '[[1, 0],[2, 1]]'
-    assert_equal expected_result, copy_random_list(node1).to_s
+    assert_equal expected_result, copyRandomList(node1).to_s
   end
 
-  def test_copy_random_list_3
+  def test_copyRandomList_3
     node31 = Node.new(3)
     node32 = Node.new(3)
     node33 = Node.new(3)
@@ -45,6 +45,6 @@ class SolutionTest < Minitest::Test
     node33.next = nil
     node33.random = nil
     expected_result = '[[3, nil],[3, 0],[3, nil]]'
-    assert_equal expected_result, copy_random_list(node31).to_s
+    assert_equal expected_result, copyRandomList(node31).to_s
   end
 end
